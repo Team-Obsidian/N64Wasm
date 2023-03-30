e280d8c5d650b051af32a178c456854ca8563249
Team-Obsidian <kepler1400@gmail.com>
2023-03-30 17:55:37 -0500

    base port

---
+++ romlist.js
@@ -0,0 +1,8 @@
+var ROMLIST = [
+
+    {url:"src/banjo-kazooie.n64",title:"Banjoo-Kazooie"},
+    {url:"src/mario-64.n64",title:"Super Mario 64"},
+    {url:"src/zelda-majora.n64",title:"Legend of Zelda: Majora's Mask"},
+    {url:"src/zelda-ocarina.n64",title:"Legend of Zelda: Ocarina of Time"},
+
+];
